0d2c7c62df70a3a3b3fef265105a605918ac9b0e
Mariusz Czarny <mariusz.czarny88@gmail.com>
2016-08-12 13:53:20 +0200

    Configuration Spring Security

---
+++ mysite/src/main/java/pl/mariuszczarny/model/User.java
@@ -0,0 +1,25 @@
+package pl.mariuszczarny.model;
+
+public final class User {
+	private final String username;
+	private final String password;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + "]";
+	}
+	
+}
