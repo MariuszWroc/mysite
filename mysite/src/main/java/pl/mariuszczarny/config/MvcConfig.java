259905df374d6cadd192637a1f5501ca21708e10
Mariusz Czarny <mariusz.czarny88@gmail.com>
2016-08-14 17:20:22 +0200

    Dalsza konfiguracja

--- mysite/src/main/java/pl/mariuszczarny/config/MvcConfig.java
+++ mysite/src/main/java/pl/mariuszczarny/config/MvcConfig.java
@@ -1,18 +1,28 @@
 package pl.mariuszczarny.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/aboutme").setViewName("aboutme");
         registry.addViewController("/login").setViewName("login");
     }
-
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
---
+++ mysite/src/main/resources/templates/aboutme.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
+<html lang="en" ng-app="StarterApp">
+	<head>
+		<title>Hack and slash MMO game</title>
+		<c:set var="contextPath" value="${pageContext.request.contextPath}" />
+		<meta name="viewport" content="initial-scale=1" />
+	</head>
+	<body layout="column">
+	test
+	</body>
+</html>
